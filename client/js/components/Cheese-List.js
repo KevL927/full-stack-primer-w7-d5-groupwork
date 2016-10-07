24bbe54189568b8521256af88068175cc488bc44
KL012 <kevl927@gmail.com>
2016-10-07 19:27:19 +0000

    Fix importing cheese array inside components/Cheese-list.js

--- client/js/components/Cheese-List.js
+++ client/js/components/Cheese-List.js
@@ -2,13 +2,13 @@ import React from 'react';
 import reactdom from 'react-dom';
 
 
-// import cheeses from '../../../server/index';
-// console.log(cheeses);
+import cheeses from '../cheeses';
+console.log(cheeses);
 
-// var CheeseList = React.createClass({
-//     render: function (props) {
-//         <ul>
-//             <li>{props.cheeses}</li>
-//         </ul>
-//     }
-// })
+var CheeseList = React.createClass({
+    render: function (props) {
+        <ul>
+            <li>{props.cheeses}</li>
+        </ul>
+    }
+});
